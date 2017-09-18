81e283e11b9d0bcacd51744b9f55184e081f9f80
Bernadine B. Juliano <trainee6@tua.edu.ph>
2017-09-18 14:41:43 +0800

    routes/class_list_crud.js

--- tes.js
+++ tes.js
@@ -9,6 +9,7 @@ const collegeCourseCrud = require('./routes/college_course_crud')(router); // Im
 const citizenshipCrud = require('./routes/citizenship_crud')(router); // Import Citizenship Crud Routes
 //const religionCrud = require('./routes/religion_crud')(router); // Import Religion Crud Routes
 const collegeCrud = require('./routes/college_crud')(router); // Import College Crud Routes
+const classListCrud = require('./routes/class_list_crud')(router); // Import College Crud Routes
 
 const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 const cors = require('cors'); // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
@@ -35,6 +36,7 @@ app.use('/collegeCourseCrud', collegeCourseCrud); // Use College Course Crud rou
 app.use('/citizenshipCrud', citizenshipCrud); // Use User Crud routes in application
 //app.use('/religionCrud', religionCrud); // Use Religion Crud routes in application
 app.use('/collegeCrud', collegeCrud); // Use College Crud routes in application
+app.use('/classListCrud', classListCrud); // Use College Crud routes in application
 // Connect server to Angular 2 Index.html
 //app.get('*', (req, res) => {
 //  res.sendFile(path.join(__dirname + '/client/index.html'));
